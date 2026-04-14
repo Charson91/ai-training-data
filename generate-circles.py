0de67f73819ce72f66ee650b392f8a1e900f6c2a
Charson91 <chr.buhl@web.de>
2026-04-14 16:37:52 +0200

    generating metadata complete

--- generate-circles.py
+++ generate-circles.py
@@ -5,11 +5,17 @@ import math
 import json
 
 r = 0.01+0.04*np.random.rand()  # random radius between 0.01 and 0.05
-r= 0.04
+#r= 0.04
 print(r)
 
 width = 1
 height = 0.8
+
+p = np.random.rand() #density
+p = 1
+
+rand_x = r*(1-2*np.random.rand()) #random starting position for the first circle, between -r and r
+rand_y = r*(1-2*np.random.rand())
 
 diag = math.sqrt(width**2 + height**2)
 
@@ -21,43 +27,81 @@ fig, ax = plt.subplots() # note we must use plt.subplots, not plt.subplot
 rand_angle = 2*np.pi*(0.5-np.random.rand())/6 #random angle for the window, one sixth because of reptition of the hexagonal pattern
 print(rand_angle)
 #rand_angle = 1*np.pi/6
-rand_angle = 0.34
+#rand_angle = 0.34
 
-rand_x = r*(1-2*np.random.rand()) #random starting position for the first circle, between -r and r
-rand_y = r*(1-2*np.random.rand())
-i = rand_x
+json_data = {} #list for collecting metadata
+
+domain = {
+        "offset x": rand_x,
+        "offset y": rand_y,
+        "width": width,
+        "height": height,
+        "angle": rand_angle
+}
+
+json_data["domain"]=domain #adding domain info to metadata
+circles = []
+
+i = rand_x  #setup for while loop
 j = rand_y
-im = i
+im = i      #mirrored coordinates because of grid path
 jm = j
-
-p = np.random.rand() #density
-#p = 1
 
 k = 0 #tracking for rows
 
 while j-r < 1.2*diag: # -r to plot cutoff circles, factor 1.2 to plot all possible corners.
     while i-r < 1.2*diag:
         if i + r > 0 and j + r > 0: #to avoid plotting circles far outside the window
-            if i - r > 0 and j - r > 0 and i + r < width and j + r < height:
-                if np.random.rand() <= p: #random generation of metadata
+            if i - r > 0 and j - r > 0 and i + r < width and j + r < height: #collecting all circles fully inside the window for metadata
+                if np.random.rand() <= p: #random generation of metadata based on density p
                     circle = plt.Circle((i, j), r, color='green')
                     ax.add_patch(circle)
+                    circle = {
+                        "circle":{
+                            "x": i,
+                            "y": j,
+                            "radius": r
+                        }
+                    }
+                    circles.append(circle)
                 else:
                     circle = plt.Circle((i, j), r, color='red')
                     ax.add_patch(circle)
+                    circle = {
+                        "circle":{
+                            "x": i,
+                            "y": j,
+                            "radius": r
+                        }
+                    }
             else:
                 circle = plt.Circle((i, j), r, color='white')
                 ax.add_patch(circle)
             if k > 0:
                 if im - r > 0 and jm - r > 0 and im + r < width and jm + r < height:
                     if np.random.rand() <= p: #mirrored circles for angled plots
+                        circle = plt.Circle((im, jm), r, color='green')
+                        ax.add_patch(circle)
+                        circle = {
+                            "circle":{
+                                "x": im,
+                                "y": jm,
+                                "radius": r
+                            }
+                        }
+                        circles.append(circle)
+                    else:
                         circle = plt.Circle((im, jm), r, color='blue')
                         ax.add_patch(circle)
-                    else:
-                        circle = plt.Circle((im, jm), r, color='orange')
-                        ax.add_patch(circle)
+                        circle = {
+                            "circle":{
+                                "x": im,
+                                "y": jm,
+                                "radius": r
+                            }
+                        }
                 else:
-                    circle = plt.Circle((im, jm), r, color='grey')
+                    circle = plt.Circle((im, jm), r, color='white')
                     ax.add_patch(circle)
 
         i += 2*r*np.cos(rand_angle) #hexgrid distance in width
@@ -66,7 +110,7 @@ while j-r < 1.2*diag: # -r to plot cutoff circles, factor 1.2 to plot all possib
         jm += 2*r*np.sin(rand_angle)
 
     k += 1
-    i = rand_x #reset position
+    i = rand_x #reset positions
     j = rand_y
     im = i
     jm = j
@@ -80,9 +124,13 @@ while j-r < 1.2*diag: # -r to plot cutoff circles, factor 1.2 to plot all possib
     jm += k*r*np.sin(rand_angle)
     jm += -k*np.sqrt(3)*r*np.cos(rand_angle)
     im += k*np.sqrt(3)*r*np.sin(rand_angle)
-    print(k, i, j)
+    #print(k, i, j)
 
- 
+json_data["circles"]=circles
+
+with open('metadata.json', 'w') as file:
+    json.dump(json_data, file, indent=4)
+#print(json_data)
 
 ax.set_xlim((0, width))
 ax.set_ylim((0, height))
